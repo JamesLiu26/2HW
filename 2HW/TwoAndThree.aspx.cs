81d27c110d3bc15e22a993332789f65b1253d2cb
晉豪 劉 <107111236@mail.oit.edu.tw>
2021-03-16 15:07:54 +0800

    Two_Three

---
+++ 2HW/TwoAndThree.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2HW
+{
+    public partial class TwoAndThree : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int N = 15, sum = 0;
+            for (int start = 1; start <= N; start++)
+            {
+                if (start % 2 == 0 || start % 3 == 0)
+                {
+                    sum += start;
+                }
+            }
+            Response.Write(sum);
+        }
+    }
+}
